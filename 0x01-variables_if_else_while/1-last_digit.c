4499159a372ea5a662e9c43c8477a259edcb6b7c
wkabia12 <wkabia12@gmail.com>
2023-02-17 06:23:00 -0800

    1.The Last Digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ *main - Prints out weather a random no is +ve,-ve or zero
+ *Return: 0
+ */
+int main(void)
+{
+	int n;
+	int rem;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	rem = n % 10
+
+	if (rem < 6 && rem != 0)
+	{
+		printf("Last digit of %d is %d and", n, rem);
+		printf("is less than 6 but not zero\n");
+	}
+	else if (rem > 5)
+	{
+		printf("Last digit of %d is %d and", n, rem);
+		printf("is greater than 5\n");
+	}
+	else if (rem == 0)
+	{
+		printf("Last digit of %d is %d and", n, rem);
+		printf("is zero\n", n);
+	}
+	return (0);
+}
