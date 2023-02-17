c8fc8e8719e4b514431d450fd500e9808ca7670e
wkabia12 <wkabia12@gmail.com>
2023-02-17 08:48:38 -0800

    6.Numbers on new line

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints 0-9 each on a new line
+ * Return: 0
+ */
+int main(void)
+{
+	char i = '0';
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
