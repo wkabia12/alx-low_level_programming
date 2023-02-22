90865b2425d7b55aebfb5f15696d9d6b791898ea
wkabia12 <wkabia12@gmail.com>
2023-02-22 06:54:41 -0800

    9 fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,6 +19,11 @@ void times_table(void)
 				_putchar(' ');
 				_putchar(' ');
 			}
+			else if (j == 9)
+			{
+				_putchar(product / 10 + '0');
+				_putchar(product % 10 + '0');
+			}
 			else if (product < 10)
 			{
 				_putchar(product + '0');
@@ -33,12 +38,6 @@ void times_table(void)
 				_putchar(',');
 				_putchar(' ');
 			}
-			else
-			{
-				_putchar('8');
-				_putchar('1');
-			}
-
 		}
 		_putchar('\n');
 	}
