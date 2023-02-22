53ff3b9bc5d301a0eb84498f9f3120073a16b817
wkabia12 <wkabia12@gmail.com>
2023-02-22 00:58:58 -0800

    Q2 fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,11 @@
 #include "main.h"
 
 /**
- * print_alphabet - print lowercase alphabet x10
- * Return: 0
+ *print_alphabet_x10 - lowercase alphabet x10
+ *
+(*
+ *Description: prints out lowercase alphabet)?
+ *Return: 0
  */
 void print_alphabet_x10(void)
 {
