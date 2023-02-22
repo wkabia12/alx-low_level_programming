65c896e1e0ad86ee33591c2f9cd593ae01eaaa6f
wkabia12 <wkabia12@gmail.com>
2023-02-22 11:58:02 -0800

    9 fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,49 +1,32 @@
 #include "main.h"
+
 /**
  * times_table - returns times table
  */
 void times_table(void)
 {
-	int i, j;
+	int i, j, result;
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 0; j <= 9; j++)
 		{
-			int product = i * j;
-
-			if (j == 0)
+			result = i * j;
+			if (result <= 9)
 			{
-				_putchar('0');
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-			}
-			else if (j == 9 && product < 10)
-			{
-				_putchar(' ');
-				_putchar(product + '0');
-			}
-			else if (product < 10)
-			{
-				_putchar(product + '0');
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-			}
-			else if (j == 9 && product > 10)
-			{
-				_putchar(product / 10 + '0');
-				_putchar(product % 10 + '0');
+				putchar(result + '0');
 			}
 			else
 			{
-				_putchar(product / 10 + '0');
-				_putchar(product % 10 + '0');
-				_putchar(',');
-				_putchar(' ');
+				putchar(result / 10 + '0');
+				putchar(result % 10 + '0');
+			}
+			if (j < 9)
+			{
+				putchar(',');
+				putchar(' ');
 			}
 		}
-		_putchar('\n');
+		putchar('\n');
 	}
 }
