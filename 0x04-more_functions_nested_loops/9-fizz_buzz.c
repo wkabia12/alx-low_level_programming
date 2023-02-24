3d17942c1151bc216ee22c96134d6ddc4d488f9d
wkabia12 <wkabia12@gmail.com>
2023-02-24 14:49:29 +0000

    Q9 and Q10 Fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,19 +12,19 @@ int main(void)
 	{
 		if (i % 15 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else
 		{
-			printf("%i ", i);
+			printf("%i", i);
 		}
 		if (i != 100)
 			printf(" ");
