de9527f0cdd88db7c447ff3aeba925ea0b198259
wkabia12 <wkabia12@gmail.com>
2023-02-24 12:46:05 +0000

    Q5 Fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,20 +5,18 @@
  */
 void more_numbers(void)
 {
-	int a = '0';
+	int a, b;
 
-	for (; a <= 15; a++)
+	for (a = 0; a < 10; a++)
 	{
-		if (a < '9')
+		for (b = 0; b <= 14; b++)
 		{
-			_putchar(a);
+			if (b > 9)
+			{
+				_putchar(b / 10 + '0');
+			}
+			_putchar(b % 10 + '0');
 		}
-		else
-		{
-			_putchar(a / 10 + '0');
-			_putchar(a % 10 + '0');
-		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
-
