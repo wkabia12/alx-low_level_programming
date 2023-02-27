3c701ad45cda5c0a8a58ce2b40cf8bb7ea996d7d
wkabia12 <wkabia12@gmail.com>
2023-02-27 20:10:28 +0000

    Q7. Print 2nd half of string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - output second half of string
+ * @str: Pointer argument
+ */
+void puts_half(char *str)
+{
+	int i = 0;
+	int len = strlen(str);
+
+	if (len % 2 == 0)
+		i = len / 2;
+	else
+		i = (len - 1) / 2;
+
+	for (; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
