7951ce355aebc55b0733741525a5c6014b81024d
wkabia12 <wkabia12@gmail.com>
2023-02-27 14:57:24 +0000

    2. Returm lenght of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * swap_int - resets number to 98
+ * @s: Pointer argument
+ * Return: string lenght
+ */
+int _strlen(char *s)
+{
+	return (strlen(*s));
+}
