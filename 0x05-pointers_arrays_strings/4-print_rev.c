3ca427fb9da58ded7b1aa2b591febaa7a465027d
wkabia12 <wkabia12@gmail.com>
2023-02-27 16:51:18 +0000

    4.Write word in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _print_rev - output reverse string followed by new line
+ * @s: Pointer argument
+ */
+void print_rev(char *s)
+{
+	int len = strlen(s);
+	int i;
+	char rev[len];
+
+	for (i = 0; i < len; i++)
+	{
+		rev[i] = s[-(i + 1)];
+	}
+	puts(rev);
+}
