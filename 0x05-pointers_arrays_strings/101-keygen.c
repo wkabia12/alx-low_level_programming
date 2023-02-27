343f1b5cbd38346af9cc580abe792c635baa830d
wkabia12 <wkabia12@gmail.com>
2023-02-27 23:00:15 +0000

    11. Keygen program

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,14 +1,28 @@
 #include "main.h"
-#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 8
 
 /**
- * _strcpy - copies string pointed to by src to dest
- * @dest: dest array
- * @src: src array
- * Return: dest pointer
+ * main - program to generate valid password
+ * Return: 0
  */
-char *_strcpy(char *dest, char *src)
+int main(void)
 {
-	strcpy(dest, src);
-	return (dest);
+	char password[PASSWORD_LENGTH + 1];
+	const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	int i = 0;
+
+	srand(time(NULL));
+
+	for (i = 0; i < PASSWORD_LENGTH; i++)
+	{
+		password[i] = charset[rand() % (sizeof(charset) - 1)];
+	}
+	password[PASSWORD_LENGTH] = '\0';
+	printf("keygen: %s\n", password);
+
+	return (0);
 }
