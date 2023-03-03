2fd472e533f4f60c5a1ee6b410050a2ad96d3fc1
wkabia12 <wkabia12@gmail.com>
2023-03-03 10:45:23 +0000

    fixes

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,11 +1,18 @@
 #include "main.h"
 #include <stdlib.h>
+#include <limits.h>
 /**
  * print_number - prints numbers
  * @n: number to be printed
  */
 void print_number(int n)
 {
+	if (n == INT_MIN)
+	{
+		_putchar('-');
+		print_number(INT_MAX / 10);
+		print_number((INT_MAX % 10) + 1);
+	}
 	if (n < 0)
 	{
 		_putchar('-');
