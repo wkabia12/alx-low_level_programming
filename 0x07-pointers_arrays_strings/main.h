8cb1f56ae42f488e6539ab30c83dafaa6bdced5a
wkabia12 <wkabia12@gmail.com>
2023-03-06 18:56:34 +0000

    2.strchr

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,5 +9,6 @@ int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 void simple_print_buffer(char *buffer, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 
 #endif /* MAIN_H */
