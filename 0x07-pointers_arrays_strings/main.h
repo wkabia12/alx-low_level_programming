0cda4248da74438f410bd813f388876ab4cf62ee
wkabia12 <wkabia12@gmail.com>
2023-03-06 18:42:53 +0000

    0.memset, 1.memcpy

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+void simple_print_buffer(char *buffer, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+
+#endif /* MAIN_H */
