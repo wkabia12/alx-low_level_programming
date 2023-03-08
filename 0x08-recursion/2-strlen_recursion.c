43d67ee3483d9174dbec2ce35197208a8b829f33
wkabia12 <wkabia12@gmail.com>
2023-03-08 18:55:10 +0000

    2, return lenght of string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - prints lenght of string
+ * @s: string to be printed
+ * Return: lenght of string
+ */
+int _strlen_recursion(char *s)
+{
+	return (strlen(s));
+}
