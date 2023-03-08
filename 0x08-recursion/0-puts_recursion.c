856893cef6247608b6ae5fb72dbb5bc09336f305
wkabia12 <wkabia12@gmail.com>
2023-03-08 17:55:08 +0000

    0,puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string followed by new line
+ * @s: string to be printed
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int _putchar(char c);
+
+#endif /* MAIN_H */
