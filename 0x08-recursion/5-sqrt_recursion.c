4cfd26044f38fc07435cf4760d10607a2787654d
wkabia12 <wkabia12@gmail.com>
2023-03-08 21:31:58 +0000

    No 5 fix

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,13 +1,16 @@
 #include "main.h"
+#include <math.h>
+int _sqrt_helper(int n, int start, int end);
 
 /**
  * _sqrt_recursion - returns natural square
+ * _sqrt_helper - sub function to find square
  * @n:number to find sqrt of
  * Return: Natural sqrt on n
  */
 int _sqrt_recursion(int n)
 {
-	if (sqrt(n) != (int) sqrt(n))
-		return (-1);
-	return (sqrt(n));
+	if (n < 0)
+	    return -1;
+	return _sqrt_helper(n, 0, n);
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -4,7 +4,6 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-#include <math.h>
 
 int _putchar(char c);
 void _puts_recursion(char *s);
@@ -14,4 +13,22 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 
+  int _sqrt_helper(int n, int start, int end)
+  {
+      int mid;
+
+      if (start > end) {
+          return -1;
+      }
+
+      mid = (start + end) / 2;
+ 
+      if (mid * mid == n) {
+          return mid;
+      } else if (mid * mid > n) {
+          return _sqrt_helper(n, start, mid - 1);
+      } else {
+          return _sqrt_helper(n, mid + 1, end);
+      }
+ }
 #endif /* MAIN_H */
