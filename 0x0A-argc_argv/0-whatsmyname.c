8f95d454dd5d8132bc95c6debda4566407f79b7f
wkabia12 <wkabia12@gmail.com>
2023-03-10 17:23:29 +0000

    0,prog that prints own name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints it's own name
+ * @argc: number of arguments passed
+ * @argv: array of arguments passed
+ */
+void main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+}
