013be1c69cbd22232113b99bda5160b857e9ddad
wkabia12 <wkabia12@gmail.com>
2023-03-13 20:22:08 +0000

    Q6, split string to words

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * strtow - return pointer to array of words
+ * @str: array of arguments
+ * Return: pointer to array of words
+ */
+char **strtow(char *str)
+{
+	char **temp, *ptr;
+	int i = 0;
+
+	if (str == NULL || str == "")
+		return (NULL);
+	temp = (char **) malloc(sizeof(char *));
+	if (temp == NULL)
+		return (NULL);
+
+	ptr = strtok(str, " ");
+
+	while (ptr != NULL)
+	{
+		temp[i] = (char *) malloc(strlen(ptr) * sizeof(char));
+		if (temp[i] == NULL)
+		{
+			free(temp);
+			return (NULL);
+		}
+		temp[i] = ptr;
+		ptr = strtok(NULL, " ");
+		i++;
+	}
+	return (temp);
+}
