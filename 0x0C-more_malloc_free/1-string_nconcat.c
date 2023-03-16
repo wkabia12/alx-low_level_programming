d6987c1113a82d033a8562c5e2185c3e307ede6f
wkabia12 <wkabia12@gmail.com>
2023-03-16 13:57:28 +0000

    1,Fix

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,30 +10,37 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ptr;
+	int s1_len, s2_len;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+
+	s1_len = strlen(s1);
+	s2_len = strlen(s2);
+
 	if (n < strlen(s2))
 	{
-		ptr = malloc(strlen(s1) + n + 1);
+		ptr = malloc(s1_len + n + 1);
 		if (ptr == NULL)
 		{
 			free(ptr);
 			return (NULL);
 		}
-		ptr = strncat(s1, s2, n);
+		strncpy(ptr, s1, s1_len);
+		strncat(ptr, s2, n);
 	}
 	else
 	{
-		ptr = malloc(strlen(s1) + strlen(s2) + 1);
+		ptr = malloc(s1_len + s2_len + 1);
 		if (ptr == NULL)
 		{
 			free(ptr);
 			return (NULL);
 		}
-		ptr = strcat(s1, s2);
+		strncpy(ptr, s1, s1_len);
+		strcat(ptr, s2);
 	}
 	return (ptr);
 }
