ca58ae12ebb17a8c3666a60de28dcc5cdc39d2a3
wkabia12 <wkabia12@gmail.com>
2023-03-20 20:46:52 +0000

    3, typedef dog_t for struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,15 +8,16 @@
  * @age: age of dog
  * @owner: name of owner
  */
-/**
- * dog_t - typedef for struct dog
- */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+};
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
