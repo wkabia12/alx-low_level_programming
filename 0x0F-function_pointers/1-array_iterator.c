62562c76a16b95dba1dbcf9f9ca15a84cdf50137
wkabia12 <wkabia12@gmail.com>
2023-03-22 13:48:22 +0000

    1, function pointer parameter looped over array elements

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes function for every element in array
+ * @array: contains elements to be printed
+ * @size: size of array
+ * @action: function pointer
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array != NULL || action != NULL)
+	{
+		unsigned int i;
+
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
+}
