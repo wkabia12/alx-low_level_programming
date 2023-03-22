1dedd399ab10fadc0ad7559bf09ba20b32921390
wkabia12 <wkabia12@gmail.com>
2023-03-22 14:13:31 +0000

    2, function that serches for integer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - executes function for every element in array
+ * @array: contains elements to be printed
+ * @size: size of array
+ * @cmp: function pointer
+ * Return: index of matched integer in array, -1 otherwise
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < (size - 1); i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
+}
