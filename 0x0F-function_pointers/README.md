9635b88c2ac946e772d5297193bbfcc70ae4f828
wkabia12 <wkabia12@gmail.com>
2023-03-22 13:20:08 +0000

    0, function ptr passed as parameter

---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+Project for C Function Pointers
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+
+#endif /* MAIN_H */
