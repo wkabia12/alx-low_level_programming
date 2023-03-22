9a6f1e5ec5097ec9093b9fc20a390c6c4897a403
wkabia12 <wkabia12@gmail.com>
2023-03-22 21:57:43 +0000

    2,Fix

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,13 +14,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (size <= 0)
 		return (-1);
 
-	if (cmp != NULL)
+	for (i = 0; i < size; i++)
 	{
-		for (i = 0; i < size; i++)
-		{
-			if (cmp(array[i]) != 0)
-				return (i);
-		}
-		return (-1);
+		if (cmp(array[i]) != 0)
+			return (i);
 	}
+	return (-1);
 }
