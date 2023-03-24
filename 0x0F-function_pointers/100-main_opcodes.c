c933776f84962a1ed671965a606b0e881d7154c6
wkabia12 <wkabia12@gmail.com>
2023-03-24 12:32:11 +0000

    4, print opcodes of of own main function

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,31 @@
+#include "function_pointers.h"
+/**
+ *main - prints opcodes of own main function
+ *@argc: number of args
+ *@argv: array of string args
+ *Return: 0-success, 1-wrong number of args, 2-number of bytes -ve
+ */
+int main(int argc, char *argv[])
+{
+	int i, num_bytes;
+	unsigned char *p;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	num_bytes = atoi(argv[1]);
+	if (num_bytes < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+	p = (unsigned char *)main;
+	for (i = 0; i < num_bytes; i++)
+	{
+		printf("%02x ", *(p+i));
+	}
+	printf("\n");
+	return 0;
+}
