ee4b421fab3877a47feafaa298d26daa9f8daf9f
wkabia12 <wkabia12@gmail.com>
2023-03-30 17:34:18 +0000

    5, execute before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * b4main - executes b4 main
+ */
+void __attribute__((constructor)) b4main()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
