1dd8bf931e34f59245601361bf4d79777efb996c
wkabia12 <wkabia12@gmail.com>
2023-05-22 11:10:47 +0000

    2, dynamic C library that can be called from python

---
+++ 0x18-dynamic_libraries/cops.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+/**
+ *add - adds 2 numbers and returns sum
+ *@a: 1st number
+ *@b: 2nd number
+ *Return: sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ *sub - subracts 2 numbers and returns sum
+ *@a: 1st number
+ *@b: 2nd number
+ *Return: diff
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ *mul - multiplys 2 numbers and returns sum
+ *@a: 1st number
+ *@b: 2nd number
+ *Return: product
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ *div - divides 2 numbers and returns sum
+ *@a: 1st number
+ *@b: 2nd number
+ *Return: dividend
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ *mod - divides 2 numbers and returns sum
+ *@a: 1st number
+ *@b: 2nd number
+ *Return: remeinder
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
